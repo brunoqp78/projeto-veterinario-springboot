d8f5ab77c02ea3038ffd6a172c31c13af2634538
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2022-10-20 22:39:54 -0300

    versão refatorada 1

--- gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/controller/HomeController.java
+++ gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/controller/HomeController.java
@@ -1,7 +1,7 @@
 package org.iftm.gerenciadorveterinarios.controller;
 
-
 import java.util.List;
+import java.util.Optional;
 
 import org.iftm.gerenciadorveterinarios.model.Veterinario;
 import org.iftm.gerenciadorveterinarios.model.repository.VeterinarioRepository;
@@ -9,23 +9,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private VeterinarioRepository vetRepository;
-    
-    
+	// O método abaixo faz injeção de dependência da classe Repository
+	@Autowired
+	private VeterinarioRepository vetRepository;
 
-    @GetMapping("/home")
-    public String home(Model model) {
-    	List<Veterinario> veterinarios = vetRepository.findAll();
-    	
-        model.addAttribute("veterinarios", veterinarios);
-        return "home";
-    }
-   
+	// Método responsável em mapear a requisição /home
+	// Ele pode receber o parâmetro nome, caso tenha sido chamado pela função de pesquisa.
+	@GetMapping("/home")
+	public String home(@RequestParam(value = "nome", required = false) Optional<String> nome, Model model) {
+		List<Veterinario> veterinarios;
+		if (nome.isPresent()) {
+			veterinarios = vetRepository.findByNomeContains(nome.get());
+		} else {
+			veterinarios = vetRepository.findAll();
+		}
+		model.addAttribute("veterinarios", veterinarios);
+		return "home";
+	}
 
 }
--- gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/controller/VeterinarioController.java
+++ gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/controller/VeterinarioController.java
@@ -1,108 +1,68 @@
 package org.iftm.gerenciadorveterinarios.controller;
 
-import java.util.List;
 import java.util.Optional;
-
-import javax.validation.Valid;
 
 import org.iftm.gerenciadorveterinarios.model.Veterinario;
 import org.iftm.gerenciadorveterinarios.model.repository.VeterinarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class VeterinarioController {
-
+	
+	// O método abaixo faz injeção de dependência da classe Repository
     @Autowired
     private VeterinarioRepository vetRepository;
 
-    // Acessa o formulario de consulta de veterinários
-    @GetMapping({"/find","/find{nome}"})
-    
-    public String veterinariosFind(@ModelAttribute("nome") @RequestParam("nome") Optional<String> nome, Model model, Veterinario veterinario) {    	
-    	if (nome.isPresent()) {
-        	System.out.println(nome.get());
-    		List<Veterinario> veterinarios = vetRepository.findByNomeContains(nome.get());
-    		System.out.println(veterinarios.size());
-    		model.addAttribute("veterinarios", veterinarios);
-    		return "home";
-    	}else {
-            return "findVeterinarioForm";    		
-    	}
+    // Faz o mapeamento para a requisição find. Acessa o formulario de consulta de veterinários    
+    @GetMapping("/find")
+    public String veterinariosFind(Model model) {
+    	return "findVeterinarioForm"; 	
     }
-
-    // Acessa o formulario de consulta de veterinários
-//    @GetMapping("/find")
-  //  public String veterinariosFindForm(Veterinario veterinario) {    	
-    //    return "findVeterinarioForm";
-    //}
     
     
-    // Acessa o formulario de cadastro de animais
+    // Faz o mapeamento para a requisição form. Acessa o formulario de cadastro de animais
     @GetMapping("/form")
     public String veterinariosForm(Veterinario veterinario) {    	
         return "addVeterinarioForm";
     }
 
-    // Adiciona novo veterinario
+    // Faz o mapeamento da requisição add. Adiciona novo veterinario
     @PostMapping("/add")
-    public String novo(@Valid Veterinario veterinario, BindingResult result) {
-
-        if (result.hasFieldErrors()) {
-            return "redirect:/form";
-        }
-
+    public String novo(Veterinario veterinario) {
         vetRepository.save(veterinario);
-
         return "redirect:/home";
     }
 
-    // Acessa o formulario de edição
+    // faz o mapeamento da requisição form com o parametro id. Acessa o formulario de edição
     @GetMapping("form/{id}")
-    public String updateForm(Model model, @PathVariable(name = "id") int id) {
-
-        //Veterinario veterinario = vetRepository.findById(id)
-        //        .orElseThrow(() -> new IllegalArgumentException("Não existe usuário com esse id: " + id));
-    	Veterinario veterinario = vetRepository.findById(id).get();
-    	if (veterinario == null) {
-    		throw new IllegalArgumentException("Não existe veterinário com esse id: " + id);
+    public String updateForm(Model model, @PathVariable int id) {
+    	Optional<Veterinario> veterinario = vetRepository.findById(id);
+    	if (veterinario.isPresent()) {
+    		model.addAttribute("veterinario", veterinario.get());
+    		return "atualizaVeterinarioForm";
+    	}else {
+    		return "redirect:/home";
     	}
-        model.addAttribute("veterinario", veterinario);
-        return "atualizaVeterinarioForm";
     }
 
-    // Atualiza veterinario
+    // faz o mapeamento da requisição update. Atualiza veterinario
     @PostMapping("update/{id}")
-    public String alterarProduto(@Valid Veterinario veterinario, BindingResult result, @PathVariable int id) {
-
-        if (result.hasErrors()) {
-            return "redirect:/form";
-        }
-        
+    public String alterarProduto(Veterinario veterinario, @PathVariable int id) {
         vetRepository.save(veterinario);
         return "redirect:/home";
     }
 
     @GetMapping("delete/{id}")
-    @CacheEvict(value = "veterinarios", allEntries = true)
-    public String delete(@PathVariable(name = "id") int id, Model model) {
-
-        //Veterinario veterinario = vetRepository.findById(id)
-        //        .orElseThrow(() -> new IllegalArgumentException("Não existe usuário com esse id: " + id));
-    	Veterinario veterinario = vetRepository.findById(id).get();
-    	if (veterinario == null) {
-    		throw new IllegalArgumentException("Não existe usuário com esse id: " + id);
-    	}
-        
-        vetRepository.delete(veterinario);
+    public String delete(@PathVariable int id) {  
+    	Optional<Veterinario> veterinario = vetRepository.findById(id);
+    	if (veterinario.isPresent()) {
+            vetRepository.delete(veterinario.get());
+    	}        
         return "redirect:/home";
     }
 
